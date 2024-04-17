c46b7aa38d149346c42be4be220d4cc756ca8314
Yejin <39898511+yejinee@users.noreply.github.com>
2024-04-17 17:29:12 +0900

    Initial commit

---
+++ src/main/java/com/campus/tadaktadak/TadaktadakApplication.java
@@ -0,0 +1,13 @@
+package com.campus.tadaktadak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadaktadakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TadaktadakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/campus/tadaktadak/TadaktadakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.tadaktadak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadaktadakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
